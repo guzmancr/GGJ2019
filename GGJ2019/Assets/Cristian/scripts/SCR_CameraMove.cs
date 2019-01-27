f21c7edb99711d3a663abecde7eb703fd07b9938
Cristian Guzman <guzmancr@usc.edu>
2019-01-26 19:57:52 -0800

    small pogram

---
+++ GGJ2019/Assets/Cristian/scripts/SCR_CameraMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_CameraMove : MonoBehaviour {
+
+
+
+    public float Movediection = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if( )
+    }
+}
