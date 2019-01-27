e02ff4cf79ca3e275494d150f52f61408df1e80d
Cristian Guzman <guzmancr@usc.edu>
2019-01-26 20:57:32 -0800

    camera following script

--- GGJ2019/Assets/Cristian/scripts/SCR_CameraMove.cs
+++ GGJ2019/Assets/Cristian/scripts/SCR_CameraMove.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class SCR_CameraMove : MonoBehaviour {
 
 
+    private LayerMask Player;
 
-    public float Movediection = 1f;
+    public float MoveMod = 1f;
+    public float MoveCam = 5f;
 
+    public GameObject CameraObject;
+    private float xStartVal;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        xStartVal = CameraObject.transform.position.x;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,8 +24,17 @@ public class SCR_CameraMove : MonoBehaviour {
 	}
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D col)
     {
-        if( )
+        if (col.gameObject.layer == Player)
+        {
+
+           // CameraObject.GetComponent<Rigidbody2D>.velocity = transform.right * MoveCam;
+           // CameraObject.transform.GetComponent<Vector2> =
+            //    =  new Vector2(transform.position.x + (MoveCam + MoveCam), 0);
+
+
+
+        }
     }
 }
---
+++ GGJ2019/Assets/SCR_SmoothCamMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_SmoothCamMove : MonoBehaviour {
+    public Transform target;
+
+    public float smoothSpeed = 0.35f; 
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        Vector3 desiredPosition = offset + target.position;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+
+	}
+}
